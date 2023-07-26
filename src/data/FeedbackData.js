297f8783e09e5d9d0546fa76bc65062a4218e9ac
Vlado Ivkovic <ivkovicv90@gmail.com>
2023-07-26 15:36:19 +0200

    Feedback App

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus saepe sit praesentium architecto placeat earum?',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus saepe sit praesentium architecto placeat earum?',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus saepe sit praesentium architecto placeat earum?',
+	},
+];
+
+export default FeedbackData
